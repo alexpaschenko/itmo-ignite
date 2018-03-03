6f99c55cfa1ced817b3a3129f15d2d8d5f976aec
Alexander Paschenko <alexander.a.paschenko@gmail.com>
2018-03-03 08:54:58 +0300

    Added Week3 module with H2 startup routine.

---
+++ Week3/src/main/java/ru/ifmo/escience/ignite/week3/H2Start.java
@@ -0,0 +1,12 @@
+package ru.ifmo.escience.ignite.week3;
+
+import org.h2.tools.Console;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+public class H2Start {
+    public static void main(String[] args) throws SQLException, IOException {
+        Console.main(args);
+    }
+}
